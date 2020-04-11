ac4afac78f1f4690e2ba5a22971588a49851832f
Captain Fantasy <captain@fantasyui.com>
2020-04-11 19:45:31 -0400

    New Release

--- plugins/postcss-reconnaissance/index.mjs
+++ plugins/postcss-reconnaissance/index.mjs
@@ -1,11 +1,10 @@
 import postcss from 'postcss';
 import fs from 'fs';
 
-const nukeit = {
+const alfamikefoxtrot = {
   'color': true,
   'background-color':true,
   'border-color':true,
-
   'border-top-color':true,
   'border-bottom-color':true,
   'border-left-color':true,
@@ -13,44 +12,21 @@ const nukeit = {
 }
 
 function payload (root, result) {
-  // console.log('root',root);
-  // console.log('result',result);
-
   root.walkAtRules(function(rule) {
-
     rule.walkDecls(function(decl, i) {
-      if(nukeit[decl.prop]){
-        console.log(`REMOVING ${decl.prop}: ${decl.value}`);
+      if(alfamikefoxtrot[decl.prop]){
         decl.remove();
       }
-
     });
   });
-
-    root.walkRules(function(rule) {
-    //console.log( rule );
-    //if(rule.selector) console.log(`Rule Selector: ${rule.selector}`);
-      rule.walkDecls(function(decl, i) {
-        //console.log(decl);
-        //console.log(`Declaration Selector: ${decl.selector}`);
-
-        //if(decl.prop) console.log(`Declaration Property: ${decl.prop}`);
-        //if(decl.value) console.log(`Declaration Value: ${decl.value}`);
-
-        //if(decl.prop.includes('color')) console.log(`${decl.prop}: ${decl.value}`);
-
-
-
-        if(nukeit[decl.prop]){
-          //console.log(`REMOVING ${decl.prop}: ${decl.value}`);
-          decl.remove();
-        }
-
-      });
+  root.walkRules(function(rule) {
+    rule.walkDecls(function(decl, i) {
+      if(alfamikefoxtrot[decl.prop]){
+        decl.remove();
+      }
     });
+  });
 }
-
-
 
 export default function(){
   return postcss.plugin('reconnaissance', function(){return payload});
